abbb17409531b59851ef372221e7661c6a0e0fc4
llomize@gmail.com <lomidze1321@gmail.com>
2022-03-28 23:17:40 +0400

    facebook(2)

---
+++ facebook/java.js
@@ -0,0 +1,10 @@
+function hiden(){
+    var pig;
+    pig = document.getElementById("open");
+    pig.classList.add("show");
+}
+function times(){
+    var pig;
+    pig=document.getElementById("open");
+    pig.classList.remove("show");
+}
