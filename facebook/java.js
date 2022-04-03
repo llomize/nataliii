f49e490852af3e411c3197ab4bcb8d538a8530c5
llomize@gmail.com <lomidze1321@gmail.com>
2022-04-03 23:40:28 +0400

    javascript first

--- facebook/java.js
+++ facebook/java.js
@@ -8,3 +8,13 @@ function times(){
     pig=document.getElementById("open");
     pig.classList.remove("show");
 }
+function wind() {
+    var windo;
+    windo = document.getElementById("show");
+    windo.classList.add("show-2");
+}
+function hi(){
+    var pig=document.getElementById("show");
+    pig.classList.remove("show-2");
+}
+
---
+++ javaScriptOne/first.js
@@ -0,0 +1,6 @@
+function damtavreba(){
+    var asaki = document.getElementById("asaki").value;
+    var saxeli = document.getElementById("saxeli").value;
+    var klasi = document.getElementById("klasi").value;
+    document.getElementById("shedegi").value = saxeli+" "+asaki+" "+klasi;
+}
