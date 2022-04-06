e92613aee16d26d610246caee5dc29be0360916f
llomize@gmail.com <lomidze1321@gmail.com>
2022-04-06 23:04:44 +0400

    facebook(3)

--- javaScriptOne/first.js
+++ javaScriptOne/first.js
@@ -1,6 +1,30 @@
 function damtavreba(){
     var asaki = document.getElementById("asaki").value;
     var saxeli = document.getElementById("saxeli").value;
-    var klasi = document.getElementById("klasi").value;
-    document.getElementById("shedegi").value = saxeli+" "+asaki+" "+klasi;
+    var chaweriliKlasi = document.getElementById("klasi").value;
+
+    var shedegiInputSpilo = document.getElementById("shedegi");
+
+    if(asaki > 18){
+        shedegiInputSpilo.value = "თქვენი ასაკი არ შეესაბამება სკოლას";
+        shedegiInputSpilo.classList.remove("warmatebit-registracia");
+        shedegiInputSpilo.classList.add("warumatebeli");
+    }
+    else {
+        shedegiInputSpilo.value = "თქვენ წარმატებით დარეგისტრირდით";
+        shedegiInputSpilo.classList.remove("warumatebeli");
+        shedegiInputSpilo.classList.add("warmatebit-registracia");
+    }
+    //
+    // var unda = asaki - 5 ;
+    // if(unda != chaweriliKlasi){
+    //     alert("არასწორად მიუთითე კლასი");
+    // }
+    //
+
+    if (chaweriliKlasi >12) {
+        alert("incorrect");
+    }
+    if(chaweriliKlasi<1)
+        alert("no");
 }
