0724fb17297c72c06b6330eaa2428bb8c8fc3a34
llomidze1321@gmail.com <llomidze1321@gmail.com>
2022-04-14 15:05:58 +0400

    stru12

--- history/mit.js
+++ history/mit.js
@@ -36,11 +36,47 @@ function next() {
     }
     if (five.classList.contains("show")) {
         five.classList.remove("show");
-        one.classList.add("show");
         return;
     }
     if (!one.classList.contains("show") && !two.classList.contains("show") &&
         !tree.classList.contains("show") && !four.classList.contains("show") && !five.classList.contains("show")) {
         one.classList.add("show");
     }
+}
+function pandora(){
+    var one = document.getElementById("panone");
+    var two = document.getElementById("pantwo");
+    var tree = document.getElementById("pantree");
+    var four = document.getElementById("panfour");
+    var five = document.getElementById("panfive");
+    if (one.classList.contains("show")){
+        two.classList.add("show");
+        one.classList.remove("show");
+        return;
+    }
+    if (!one.classList.contains("show") && !two.classList.contains("show") && !tree.classList.contains("show") &&
+        !four.classList.contains("show") && !five.classList.contains("show")){
+    one.classList.add("show");
+    return;
+    }
+    if (two.classList.contains("show")) {
+        one.classList.remove("show");
+        two.classList.remove("show");
+        tree.classList.add("show");
+        four.classList.remove("show");
+
+        return;
+    }
+    if(tree.classList.contains("show")){
+        tree.classList.remove("show");
+        four.classList.add("show");
+    }
+    if(four.classList.contains("show")){
+        four.classList.remove("show");
+        five.classList.add("show");
+    }
+    if(five.classList.contains("show")){
+        five.classList.remove("show");
+    }
+
 }
