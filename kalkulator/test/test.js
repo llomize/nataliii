f5fae00fcda3342a9295161f04060c169d6e18b0
llomidze1321@gmail.com <llomidze1321@gmail.com>
2022-05-05 15:58:28 +0400

    stru12

--- kalkulator/test/test.js
+++ kalkulator/test/test.js
@@ -5,3 +5,12 @@ function change(){
     input1.value = input2.value;
     input2.value = saveinput1;
 }
+function change2(){
+    var input1 = document.getElementById("id1");
+    var input2 = document.getElementById("id2");
+    var input3 = document.getElementById("id3");
+    var saveid1= input2.value;
+    input2.value = input1.value;
+    input1.value = input3.value;
+    input3.value = saveid1;
+}
