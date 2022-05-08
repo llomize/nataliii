b44b870a3e18488fdf6b84d2abf241112d57c98e
llomidze1321@gmail.com <llomidze1321@gmail.com>
2022-05-08 13:01:18 +0400

    stru12

--- kalkulator/form/form.js
+++ kalkulator/form/form.js
@@ -1,31 +1,38 @@
-function cl(){
+function cl() {
     var input1 = document.getElementById("one");
     var input2 = document.getElementById("two");
     var input3 = document.getElementById("tree");
     var input4 = document.getElementById("four");
     var input5 = document.getElementById("five");
-    if(input1.value == "") {
+    if (input1.value == "") {
         alert("დაწერეთ თქვენი სახელი");
         return;
     }
-    if(input2.value == ""){
+    if (input2.value == "") {
         alert("lastname");
         return;
     }
-    if(input3.value == ""){
+    if (input3.value == "") {
         alert("how old are you");
         return;
     }
-    if(input4.value == ""){
+    if (input4.value == "") {
         alert("e-mail");
         return;
     }
-    if(input5.value == ""){
+    if (input5.value == "") {
         alert("password");
         return;
     }
 
 
-    alert("seccess");
 
+    if (!input4.value.includes("@")) {
+        alert("check");
+    }
+    alert("seccess");
+    if(input3.value){
+
+    }
 }
+
