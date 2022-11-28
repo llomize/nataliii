8cb330208e84619ab840d67ef17cea013f74557c
lomid <lomidze1321@gmail.com>
2022-11-28 02:59:18 -0800

    stru12

---
+++ form/js.js
@@ -0,0 +1,26 @@
+var masivi = [102,10,49,18,10,59,5];
+console.log(masivi[0]);
+if(masivi.length%2 == 0){
+    console.log(masivi[masivi.length/2-1]);
+    console.log(masivi[masivi.length/2]);
+}else {
+    console.log(masivi[Math.floor(masivi.length/2)])
+}
+console.log(masivi[masivi.length-1]);
+
+
+
+
+var one = prompt("ასაკი")
+var two = prompt("სქესი")
+
+if(one>0 && one<18){
+    console.log("მოზარდი")
+
+}
+
+else if(one<60){
+    console.log("ზრდასრული")
+}
+else if(one=>65 && two==კაცი || one=>65 && two==ქალი)
+
